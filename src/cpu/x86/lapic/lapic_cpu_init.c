fb703ca7eb8e5a2db412272087f65bd6bf55ebba
John Lewis <jlewis@johnlewis.ie>
2015-06-02 10:07:21 +0100

    Added "__attribute__((regparm(0)))" to lapic_cpu_init.c to avoid "conflicting types" compile error, as per a571c70c14a86d242be39fc12610b2519499379f

--- src/cpu/x86/lapic/lapic_cpu_init.c
+++ src/cpu/x86/lapic/lapic_cpu_init.c
@@ -421,7 +421,7 @@ static __inline__ __attribute__((always_inline)) void writecr4(unsigned long Dat
 #endif
 
 /* C entry point of secondary cpus */
-void secondary_cpu_init(unsigned int index)
+void __attribute__((regparm(0))) secondary_cpu_init(unsigned int index)
 {
 	atomic_inc(&active_cpus);
 #if CONFIG_SERIAL_CPU_INIT
